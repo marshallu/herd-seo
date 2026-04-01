89551c032db02de188e02df8861da3171e779151
Christopher McComas <mccomas.chris@gmail.com>
2026-04-01 10:28:42 -0400

    Bump version to 1.0.2

--- mu-seo.php
+++ mu-seo.php
@@ -9,7 +9,7 @@
  * Plugin Name:  MU SEO
  * Plugin URI: https://www.marshall.edu
  * Description: This is a lean SEO plugin for Marshall University.
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: Christopher McComas
  */
 
